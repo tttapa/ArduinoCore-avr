c89921a3e899e9ee2f4ac9b92dd3689df39ccfd3
tttapa <pieter.pas@outlook.com>
2018-06-07 19:01:38 +0200

    Update Arduino.h

--- cores/arduino/Arduino.h
+++ cores/arduino/Arduino.h
@@ -231,6 +231,7 @@ extern const uint8_t PROGMEM digital_pin_to_timer_PGM[];
 #include "WString.h"
 #include "HardwareSerial.h"
 #include "USBAPI.h"
+#include "Debug.hpp"
 #if defined(HAVE_HWSERIAL0) && defined(HAVE_CDCSERIAL)
 #error "Targets with both UART0 and CDC serial not supported"
 #endif
